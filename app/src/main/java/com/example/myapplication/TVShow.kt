d2ffb516b6b5d44e6bbe79dd2b3cf4bbb010dd0b
Mohit Makhija <113151288+mohitmakhija8612@users.noreply.github.com>
2024-07-07 20:41:59 -0400

    Update TVShow.kt

--- app/src/main/java/com/example/myapplication/TVShow.kt
+++ app/src/main/java/com/example/myapplication/TVShow.kt
@@ -1,11 +1,24 @@
-package com.example.myapplication
+package ca.georgiancollege.ice7
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-//Model class
+//model class
+// Entity annotation marks this class as a table in the Room Database
+@Entity(tableName = "tv_shows")
 data class TVShow(
+    // PrimaryKey annotation marks this field as the primary key
+    // autoGenerate = true indicates that the field should be auto-generated
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
 
+    // Column to store the title of the TVShow
     val title: String,
-    val subTitle: String
+
+    // Column to store the genre of the TVShow
+    val genre: String,
+
+    // Column to store the rating of the TVShow
+    val rating: Double
 )
+
